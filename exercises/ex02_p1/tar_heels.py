4462c687673b991f46bcf3bed059a38da44b98e6
Taylor Watts <taylor_watts@med.unc.edu>
2021-02-09 21:08:03 -0500

    fix linting

--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -2,15 +2,17 @@
 
 __author__ = "720332576"
 
+
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
     # TODO 2: Print the response of calling the tar_heels function here.
     print(tar_heels(choice))
 
+
 # TODO 1: Define the tar_heels function, and its logic, here.
 def tar_heels(z:int) -> str:
-#takes input and does mod to see what str to return
+    """takes input and does mod to see what str to return"""
     a = z % 2
     b = z % 7
 
@@ -27,5 +29,3 @@ def tar_heels(z:int) -> str:
 
 if __name__ == "__main__":
     main()
-
-    
