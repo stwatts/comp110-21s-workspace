07649d40abd75d16326aa421d031f80fc73a8e98
Taylor Watts <taylor_watts@med.unc.edu>
2021-03-02 20:27:13 -0500

    part 2 try 1

--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -1,12 +1,38 @@
 """EX03 - palindromify function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "720332576"
 
+
+def palindromify(x: str, y: bool) -> str:
+    """Palindromifies your string."""
+    mylist = []
+    reverselist = []
+    i = 0
+    a = len(x)
+    if y == True:
+        while i < a:
+            mylist.insert(i, x[i])
+            reverselist.insert(i, x[i])
+            i += 1
+        reverselist.reverse()
+        palin = mylist + reverselist
+        return ''.join(palin)
+    else:
+        while i < a:
+            mylist.insert(i, x[i])
+            reverselist.insert(i, x[i])
+            i += 1
+        reverselist.reverse()
+        reverselist.pop(0)
+        palin = mylist + reverselist
+        return ''.join(palin)
+       
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(palindromify("race", false))
+    a = str(input())
+    b = (len(a) - 1) % 2
+    print(palindromify(a,b))
 
 
 if __name__ == "__main__":
